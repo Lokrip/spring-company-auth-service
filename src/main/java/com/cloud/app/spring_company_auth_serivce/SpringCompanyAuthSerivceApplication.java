721322d092ae837e522a80c8cc92779a7617533c
Lokrip <slava.dorokhov.08@mail.ru>
2025-04-29 09:52:15 +0600

    init: initialization of the auth serivce

---
+++ src/main/java/com/cloud/app/spring_company_auth_serivce/SpringCompanyAuthSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.app.spring_company_auth_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCompanyAuthSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCompanyAuthSerivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/app/spring_company_auth_serivce/SpringCompanyAuthSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.app.spring_company_auth_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompanyAuthSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
